90dad0800524f1446780fcdcddee79cf2fdfdf37
George Dilthey <george.dilthey@gmail.com>
2021-07-31 14:37:37 -0400

    commit tree 2a48d8a2e31fde6080cb8744db48c01eb6cfddcb

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    let newArr = []
+    for(let i=0; i<arr.length; i++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr;
+}
+
+function countDown(int) {
+    while (int>-1){
+        console.log(int)
+        int-=1
+    }
+}
